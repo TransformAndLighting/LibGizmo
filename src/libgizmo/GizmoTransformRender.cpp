40c724866e6da55f8f937f8c9ff379587170a0b7
Marco Di Benedetto <marcodibenedetto1979@gmail.com>
2018-02-27 19:14:05 +0100

    additional drawing fixes.

--- src/libgizmo/GizmoTransformRender.cpp
+++ src/libgizmo/GizmoTransformRender.cpp
@@ -297,7 +297,8 @@ void CGizmoTransformRender::DrawQuad(const tvector3 &orig, float size, bool bSel
     tmp    = pts[2];
     pts[2] = pts[3];
     pts[3] = tmp;
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(pts[0]), 0);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, GLsizeiptr(sizeof(pts)), pts);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(pts[0]), 0);
     glDrawArrays(GL_LINE_LOOP, 0, sizeof(pts) / sizeof(pts[0]));
     glDisable(GL_BLEND);
     //glEnable(GL_DEPTH_TEST);
